f9ade22e8700b099554c267e87fa2dd711136603
= <post@madebyrolf.nl>
2014-09-03 23:02:45 +0200

    First steps for this library.

---
+++ example/index.js
@@ -0,0 +1,15 @@
+
+var IRCConnection = require('../index.js').Connection;
+
+var con = new IRCConnection({
+    host: 'irc.freenode.net',
+    port: '6665',
+    nick: 'RolfTest',
+    realname: 'Rolf',
+    ident: 'rolf'
+});
+
+con.connect();
+con.on('connected', function(data) {
+    console.log('Connected');
+});
